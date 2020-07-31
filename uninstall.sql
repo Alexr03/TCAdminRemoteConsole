daa746e98e6c7fd3a16f7dfb13f2494c1c3e9c3d
Alex Redding <Alexredding180@gmail.com>
2020-07-31 19:06:40 +0100

    Update sql

--- uninstall.sql
+++ uninstall.sql
@@ -1,4 +1,3 @@
 ﻿DROP TABLE tcmodule_terminal_scripts;
 
-DELETE FROM tc_site_map WHERE module_id LIKE '280ea300-c313-46f8-90e5-5d0cc27ea7cf';
-DELETE FROM tc_modules WHERE module_id LIKE '280ea300-c313-46f8-90e5-5d0cc27ea7cf';
+DELETE FROM tc_site_map WHERE controller LIKE 'RemoteConsole';
